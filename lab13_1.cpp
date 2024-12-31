aa1f51e8b62fa440e572c6392e49ac705e89a541
TIM <86812783+Theejuth@users.noreply.github.com>
2024-12-31 12:08:24 +0700

    LAB13_1 COMPLETE!

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N, double B[6]){
+    double sum = 0,sum_2 = 0,sum_3 = 0,geo = 1,max = A[0],min = A[0];
+    for(int i=0;i < N; i++){
+        sum += A[i];
+    }
+    
+    B[0] = sum/N;
+    
+    for(int i=0;i < N; i++){
+        if (A[i] > max){
+            max = A[i];
+        }
+        
+        if (A[i] < min){
+            min = A[i];
+        }
+        
+        sum_2 += pow((A[i]-B[0]),2);
+        sum_3 += 1/A[i];
+        geo *= A[i];
+    }
+    B[1] = sqrt(sum_2/N);
+    B[2] = pow(geo,(1.0/6.0));
+    B[3] = N/sum_3;
+    B[4] = max;
+    B[5] = min;
+}
